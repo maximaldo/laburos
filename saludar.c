2a91da5985b769fc635f1369ac440bfddf3fb939
maximaldo <130598408+maximaldo@users.noreply.github.com>
2023-05-18 08:58:13 -0300

    Update saludar.c

--- saludar.c
+++ saludar.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void saludar(char saludo) {
+void saludar(char *saludo) {
     printf("Hola, %s.\n", saludo);
 }
 
 int main(int argc, char *argv[]) {
-    char saludo = argv[1];
+    char *saludo = argv[1];
     saludar(saludo);
     return 0;
 }
